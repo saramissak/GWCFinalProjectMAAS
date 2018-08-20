89275c50689f9e5161eb5805cfe5bf1f0b4567fb
Madison <42008799+codenamemadison@users.noreply.github.com>
2018-08-20 16:03:12 -0400

    Edit Profile More Options
    added more options to profile

--- editProfileTTEESSTT.js
+++ editProfileTTEESSTT.js
@@ -20,19 +20,91 @@ function changePrefs(){
 	var lastName = document.getElementById('lastName-box').value;
 	var age = document.getElementById('age-box').value;
   var gender = document.getElementById('gender-box').value;
+  var address = document.getElementById('address-box').value;
+  var phoneNumber = document.getElementById('phoneNumber-box').value;
+  var email = document.getElementById('email-box').value;
+  var objective = document.getElementById('objective-box').value;
+  var skills = document.getElementById('skills-box').value;
+  var accomplishments = document.getElementById('accomplishments-box').value;
+
+  // Racial Identity checklist
+  print(document.getElementById("LatinoOrHispanic").value);
 	if (user) {
 		var uid = user.uid;
-		setPref(uid, firstName, lastName, age, gender);
-	}
+
+
+    ///////////////// RETREIVING THE DATA //////////////////////
+    // gives the user the ability to change one section rathe than
+    // having to reinput all the values in
+
+    // builds the location of the data
+    var ref = database.ref('/user_prefs/' + uid);
+
+    // gets the data
+    return ref.once('value').then(function(snapshot){
+    	// when it's successful, get the value JSON
+    	var my_pref = snapshot.val();
+
+      // if user does not put in new input, get the previous values in the JSON
+      if (!firstName) {
+        firstName = my_pref.firstName;
+      }
+      if (!lastName) {
+        lastName = my_pref.lastName;
+      }
+      if (!age) {
+        age = my_pref.age;
+      }
+      if (!gender) {
+        gender = my_pref.gender;
+      }
+      if (!address) {
+        address = my_pref.address;
+      }
+      if (!phoneNumber) {
+        phoneNumber = my_pref.phoneNumber;
+      }
+      if (!email) {
+        email = my_pref.email;
+      }
+      if (!objective) {
+        objective = my_pref.objective;
+      }
+      if (!skills) {
+         skills = my_pref.skills;
+      }
+      if (!accomplishments) {
+        accomplishments = my_pref.accomplishments;
+      }
+      alert(firstName);
+      alert(lastName);
+      alert(age);
+      alert(gender);
+      alert(address);
+      alert(phoneNumber);
+      alert(email);
+      alert(objective);
+      alert(skills);
+      alert(accomplishments);
+
+      setPref(uid, firstName, lastName, age, gender, address, phoneNumber, email, objective, skills, accomplishments);
+	  });
+
+  }
 }
 
-
-function setPref(uid, firstName, lastName, age, gender){
+function setPref(uid, firstName, lastName, age, gender, address, phoneNumber, email, objective, skills, accomplishments){
 	var prefs = {
 		firstName: firstName,
 		lastName: lastName,
     age: age,
     gender: gender,
+    address: address,
+    phoneNumber: phoneNumber,
+    email: email,
+    objective: objective,
+    skills: skills,
+    accomplishments: accomplishments,
 	};
 
 	var ref = database.ref('/user_prefs/' + uid);
@@ -55,17 +127,35 @@ function getPref(uid){
 		var lastName = my_pref.lastName;
     var age = my_pref.age;
     var gender = my_pref.gender;
+    var address = my_pref.address;
+    var phoneNumber = my_pref.phoneNumber;
+    var email = my_pref.email;
+    var objective = my_pref.objective;
+    var skills = my_pref.skills;
+    var accomplishments = my_pref.accomplishments;
 
 		// change the display
 		document.getElementById('firstName').innerHTML = my_pref.firstName;
 		document.getElementById('lastName').innerHTML = my_pref.lastName;
     document.getElementById('age').innerHTML = my_pref.age;
     document.getElementById('gender').innerHTML = my_pref.gender;
+    document.getElementById('address').innerHTML = my_pref.address;
+    document.getElementById('phoneNumber').innerHTML = my_pref.phoneNumber;
+    document.getElementById('email').innerHTML = my_pref.email;
+    document.getElementById('objective').innerHTML = my_pref.objective;
+    document.getElementById('skills').innerHTML = my_pref.skills;
+    document.getElementById('accomplishments').innerHTML = my_pref.accomplishments;
     // change the placeholder
     document.getElementById('firstName-box').placeholder = firstName;
     document.getElementById('lastName-box').placeholder = lastName;
     document.getElementById('age-box').placeholder = age;
     document.getElementById('gender-box').placeholder = gender;
+    document.getElementById('address-box').placeholder = address;
+    document.getElementById('phoneNumber-box').placeholder = phoneNumber;
+    document.getElementById('email-box').placeholder = email;
+    document.getElementById('objective-box').placeholder = objective;
+    document.getElementById('skills-box').placeholder = skills;
+    document.getElementById('accomplishments-box').placeholder = accomplishments;
 	});
 }
 
@@ -82,4 +172,4 @@ function authStatusListener() {
 	});
 }
 
-window.onload = function() { authStatusListener();};
+window.onload = function(){authStatusListener();};
